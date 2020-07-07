ce4d0409ab85052c1f0a9b4c9cd2f6a3cab2d31e
sharon200102 <sharon200102@gmail.com>
2020-07-07 08:00:40 +0300

    repeted the analysis for CRC_only and uploaded its results

--- Projects/CRC_data/Main.py
+++ Projects/CRC_data/Main.py
@@ -29,10 +29,12 @@ mapping_table=pd.read_csv(Constants.maping_page_url)
 """Because the deficiency in TP4 samples TP 5 will ve treated as TP4"""
 mapping_table_with_new_time_target=mapping_table.assign(TimePointNum=mapping_table['TimePointNum'].replace(Constants.old_target_time,Constants.target_time))
 merged_table=pd.merge(samples_results,mapping_table_with_new_time_target,on='#SampleID')
+only_crc_merged_table=merged_table.loc[merged_table['Group']=='CRC']
+only_crc_merged_table.reset_index(drop=True,inplace=True)
 """Get and select the wanted TimePoints"""
 TimePoint=input('Enter the time points to perform on  \n'+"\n".join(Constants.TimePoints_list)+"\n")
 if TimePoint=='0':
-    merged_table=merged_table[merged_table['TimePointNum']==0]
+    only_crc_merged_table=only_crc_merged_table[only_crc_merged_table['TimePointNum']==0]
 elif TimePoint!='All':
     raise ValueError
 
@@ -44,26 +46,27 @@ identification is by CageNum MiceNum and Experiment
 idx = 0
 multi_class_tumor_load = []
 
-for cage, mice, exp in zip(merged_table['CageNum'], merged_table['MiceNum'], merged_table['Experiment']):
+for cage, mice, exp in zip(only_crc_merged_table['CageNum'], only_crc_merged_table['MiceNum'], only_crc_merged_table['Experiment']):
 
     dic = {'CageNum': cage, 'MiceNum': mice, 'Experiment': exp, 'TimePointNum': Constants.target_time}
-    item = SA.conditional_identification(merged_table, dic)['tumor_load'].values
+    item = SA.conditional_identification(only_crc_merged_table, dic)['tumor_load'].values
     if (len(item) == 0):
-        merged_table.drop(idx, inplace=True)
+        only_crc_merged_table.drop(idx, inplace=True)
     else:
         multi_class_tumor_load.append(item[0])
     idx += 1
 
 multi_class_tumor_load = pd.Series(multi_class_tumor_load)
-binary_tumor_load=multi_class_tumor_load.apply(lambda x: 1 if x>0 else 0)
+tumor_median=multi_class_tumor_load.median()
+binary_tumor_load=multi_class_tumor_load.apply(lambda x: 1 if x>tumor_median else 0)
 
-merged_table.reset_index(drop=True,inplace=True)
+only_crc_merged_table.reset_index(drop=True,inplace=True)
 """
 Drop all columns that are not related to bacterial information.
 """
 
-samples_bacterial_data=merged_table.drop(Constants.mapping_and_general_info_columns,axis=1)
-samples_bacterial_data_and_identification=merged_table.drop(Constants.mapping_and_general_info_columns_no_identification,axis=1)
+samples_bacterial_data=only_crc_merged_table.drop(Constants.mapping_and_general_info_columns,axis=1)
+samples_bacterial_data_and_identification=only_crc_merged_table.drop(Constants.mapping_and_general_info_columns_no_identification,axis=1)
 """remove bacteria that only consist of zero """
 SA.removeZeroCols(samples_bacterial_data)
 SA.removeZeroCols(samples_bacterial_data_and_identification)
@@ -75,10 +78,10 @@ biit.export_edges_to_csv('Edges of Bacteria_interaction_network ')
 
 """Plot correlation between the bacteria and the target, correlation between immune_system_features and the target """
 
-"""
+
 Plot.draw_rhos_calculation_figure(multi_class_tumor_load,samples_bacterial_data.drop('#SampleID',axis=1),'Correlation between bacteria and target',6,save_folder='Graphs\Correlation\Bacteria')
-Plot.draw_rhos_calculation_figure(merged_table['tumor_load'],merged_table[Constants.immune_system_features],'Correlation between immune system parameters and the target',6,save_folder='Graphs\Correlation\Others')
-"""
+Plot.draw_rhos_calculation_figure(only_crc_merged_table['tumor_load'],only_crc_merged_table[Constants.immune_system_features],'Correlation between immune system parameters and the target',6,save_folder='Graphs\Correlation\Others')
+
 
 """Remove highly correlated columns"""
 uncorr_data=SA.dropHighCorr(samples_bacterial_data,Constants.THRESHOLD)
@@ -92,27 +95,26 @@ dimension_fn_name=input('Enter the dimensionality reduction function wanted \n P
 dec_obj,dec_data=decompose(normalized_data,Constants.dimension_reduction_dict[dimension_fn_name],n_components=5,random_state=1)
 
 """Visualizations after decomposition, different visualizations will be performed based on initially selected time points"""
-labels_dict={1:'Tumor',0:'No tumor'}
+labels_dict={1:'More tumors than median',0:'Less tumors than median'}
 title='Data after {fn} relationship between features '.format(fn=dimension_fn_name)
 Plot.relationship_between_features(dec_data,folder='Graphs',color=binary_tumor_load,title=title,labels_dict=labels_dict)
 
 if TimePoint=='All':
     title='Progress in time of column attribute mean {fn}'.format(fn=dimension_fn_name)
-    p1=Plot.progress_in_time_of_column_attribute_mean(dec_data,merged_table['TimePointNum'],binary_tumor_load)
+    p1=Plot.progress_in_time_of_column_attribute_mean(dec_data,only_crc_merged_table['TimePointNum'],binary_tumor_load,margin=0.1)
     new_plot=p1.plot()
     new_plot.set_title(title)
     new_plot.set_xlabel('Time')
     new_plot.set_ylabel('Mean Value')
     plt.show()
 """Look only at time point zero"""
-dec_data_at0=dec_data[merged_table['TimePointNum']==0]
-
+dec_data_at_time_of_prediction=dec_data[only_crc_merged_table['TimePointNum']==Constants.TIME_OF_PREDICTION]
 kind_of_prediction=input('Enter whether you want \n'+"\n".join(Constants.prediction_sort)+"\n")
 
 if kind_of_prediction=='Binary':
-    class_list_at0=binary_tumor_load[merged_table['TimePointNum']==0]
+    class_list_at_time_of_prediction=binary_tumor_load[only_crc_merged_table['TimePointNum']==Constants.TIME_OF_PREDICTION]
 elif kind_of_prediction=='Multi' or kind_of_prediction=='Regression' :
-    class_list_at0=multi_class_tumor_load[merged_table['TimePointNum']==0]
+    class_list_at_time_of_prediction=multi_class_tumor_load[only_crc_merged_table['TimePointNum']==Constants.TIME_OF_PREDICTION]
 else:
     raise ValueError
 """Split the data and classify it"""
@@ -125,15 +127,15 @@ if kind_of_prediction!='Regression':
     """Evaluate the KNN for various K values. """
     for neighbors in range(1,Constants.MAX_NIGH):
         neigh = KNeighborsClassifier(n_neighbors=neighbors,weights='distance',p=1)
-        percentage.append(Clustering.cross_validation(kf,dec_data_at0,class_list_at0,neigh,eval_fn_name)[1])
+        percentage.append(Clustering.cross_validation(kf,dec_data_at_time_of_prediction,class_list_at_time_of_prediction,neigh,eval_fn_name)[1])
 
 
     """Plot the results"""
     plt.plot(range(1,Constants.MAX_NIGH),percentage,marker='o',markersize=15,markerfacecolor='red')
     plt.title('Data TimePoints : {TP}\nModel : {pred} KNN \nDimension_fn : {dim} \nCorrelation_threshold : {'
-'corr_TH}\nNormalization_fn : {norm}\n'.format(TP=TimePoint,pred=kind_of_prediction,
+'corr_TH}\nNormalization_fn : {norm}\nTumor median : {median}\nPrediction time : {pred_time}\n'.format(TP=TimePoint,pred=kind_of_prediction,
                                                               dim=dimension_fn_name,corr_TH=str(Constants.THRESHOLD),
-                                                              norm=normalization_fn_name))
+                                                              norm=normalization_fn_name,median=tumor_median,pred_time=Constants.TIME_OF_PREDICTION))
     plt.ylabel(eval_fn_name)
     plt.xlabel('K neighbors')
     plt.tight_layout()
@@ -146,7 +148,7 @@ elif kind_of_prediction=='Regression':
     train_loss=[]
     for C in [0+p/100 for p in range(1,10000)]:
         clf= SVR(C=C, epsilon=0.1,kernel='poly',degree=3,gamma='scale')
-        result=Clustering.cross_validation(kf,dec_data_at0,class_list_at0,clf,eval_fn_name)
+        result=Clustering.cross_validation(kf,dec_data_at_time_of_prediction,class_list_at_time_of_prediction,clf,eval_fn_name)
         train_loss.append(result[0])
         test_loss.append((result[1]))
     plt.plot([0+p/100 for p in range(1,10000)],train_loss,marker='o',markersize=5,label='Train')
