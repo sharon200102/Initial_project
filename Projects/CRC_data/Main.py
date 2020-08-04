9ac3774bdafb8b64a8a0cbb03a2e9508f1a82bdf
sharon200102 <sharon200102@gmail.com>
2020-08-04 14:13:56 +0300

    allowing also only CRC option

--- Projects/CRC_data/Main.py
+++ Projects/CRC_data/Main.py
@@ -30,17 +30,17 @@ mapping_table=pd.read_csv(Constants.maping_page_url)
 mapping_table_with_new_time_target=mapping_table.assign(TimePointNum=mapping_table['TimePointNum'].replace(Constants.old_target_time,Constants.target_time))
 merged_table=pd.merge(samples_results,mapping_table_with_new_time_target,on='#SampleID')
 """Get and select the wanted TimePoints"""
-TimePoint=input('Enter the time points to perform on  \n'+"\n".join(Constants.TimePoints_list)+"\n")
-if TimePoint=='0':
-    merged_table=merged_table[merged_table['TimePointNum']==0]
-elif TimePoint!='All':
+Group=input('Enter the groups that you would like to perform the analysis on  \n'+"\n".join(Constants.Groups_list)+"\n")
+if Group =='CRC':
+    merged_table=merged_table.loc[merged_table['Group']=='CRC']
+    merged_table.reset_index(drop=True, inplace=True)
+elif Group!='All':
     raise ValueError
 
 """
 Add classification of T5 to all samples
 identification is by CageNum MiceNum and Experiment 
 """
-
 idx = 0
 multi_class_tumor_load = []
 
@@ -55,9 +55,14 @@ for cage, mice, exp in zip(merged_table['CageNum'], merged_table['MiceNum'], mer
     idx += 1
 
 multi_class_tumor_load = pd.Series(multi_class_tumor_load)
-binary_tumor_load=multi_class_tumor_load.apply(lambda x: 1 if x>0 else 0)
+if Group=='All':
+    binary_tumor_load=multi_class_tumor_load.apply(lambda x: 1 if x>0 else 0)
 
+elif Group=='CRC':
+    median=multi_class_tumor_load.median()
+    binary_tumor_load=multi_class_tumor_load.apply(lambda x: 1 if x>median else 0)
 merged_table.reset_index(drop=True,inplace=True)
+
 """
 Drop all columns that are not related to bacterial information.
 """
@@ -67,18 +72,22 @@ samples_bacterial_data_and_identification=merged_table.drop(Constants.mapping_an
 """remove bacteria that only consist of zero """
 SA.removeZeroCols(samples_bacterial_data)
 SA.removeZeroCols(samples_bacterial_data_and_identification)
-"""Plot a dynamic graph"""
-biit=Plot.bacteria_intraction_in_time(samples_bacterial_data_and_identification,Constants.identification_columns,'TimePointNum')
-v = [100] * len(biit.relevant_columns)
-biit.plot(node_size_list=v,G_name='Bacteria_interaction_network',folder='Bacteria_interaction_network')
-biit.export_edges_to_csv('Edges of Bacteria_interaction_network ')
 
-"""Plot correlation between the bacteria and the target, correlation between immune_system_features and the target """
+correlation_plots=False
+if correlation_plots:
+    """Plot a dynamic graph"""
 
-"""
-Plot.draw_rhos_calculation_figure(multi_class_tumor_load,samples_bacterial_data.drop('#SampleID',axis=1),'Correlation between bacteria and target',6,save_folder='Graphs\Correlation\Bacteria')
-Plot.draw_rhos_calculation_figure(merged_table['tumor_load'],merged_table[Constants.immune_system_features],'Correlation between immune system parameters and the target',6,save_folder='Graphs\Correlation\Others')
-"""
+    biit=Plot.bacteria_intraction_in_time(samples_bacterial_data_and_identification,Constants.identification_columns,'TimePointNum')
+    v = [100] * len(biit.relevant_columns)
+    biit.plot(node_size_list=v,G_name='Bacteria_interaction_network',folder='Bacteria_interaction_network')
+    biit.export_edges_to_csv('Edges of Bacteria_interaction_network ')
+
+    """Plot correlation between the bacteria and the target, correlation between immune_system_features and the target """
+
+
+    Plot.draw_rhos_calculation_figure(multi_class_tumor_load,samples_bacterial_data.drop('#SampleID',axis=1),'Correlation between bacteria and target',6,save_folder='Graphs\Correlation\Bacteria')
+    Plot.draw_rhos_calculation_figure(merged_table['tumor_load'],merged_table[Constants.immune_system_features],'Correlation between immune system parameters and the target',6,save_folder='Graphs\Correlation\Others')
+
 
 """Remove highly correlated columns"""
 uncorr_data=SA.dropHighCorr(samples_bacterial_data,Constants.THRESHOLD)
@@ -96,14 +105,13 @@ labels_dict={1:'Tumor',0:'No tumor'}
 title='Data after {fn} relationship between features '.format(fn=dimension_fn_name)
 Plot.relationship_between_features(dec_data,folder='Graphs',color=binary_tumor_load,title=title,labels_dict=labels_dict)
 
-if TimePoint=='All':
-    title='Progress in time of column attribute mean {fn}'.format(fn=dimension_fn_name)
-    p1=Plot.progress_in_time_of_column_attribute_mean(dec_data,merged_table['TimePointNum'],binary_tumor_load)
-    new_plot=p1.plot()
-    new_plot.set_title(title)
-    new_plot.set_xlabel('Time')
-    new_plot.set_ylabel('Mean Value')
-    plt.show()
+title='Progress in time of column attribute mean {fn}'.format(fn=dimension_fn_name)
+p1=Plot.progress_in_time_of_column_attribute_mean(dec_data,merged_table['TimePointNum'],binary_tumor_load)
+new_plot=p1.plot()
+new_plot.set_title(title)
+new_plot.set_xlabel('Time')
+new_plot.set_ylabel('Mean Value')
+plt.show()
 """Look only at time point zero"""
 dec_data_at0=dec_data[merged_table['TimePointNum']==0]
 
@@ -111,7 +119,7 @@ kind_of_prediction=input('Enter whether you want \n'+"\n".join(Constants.predict
 
 if kind_of_prediction=='Binary':
     class_list_at0=binary_tumor_load[merged_table['TimePointNum']==0]
-elif kind_of_prediction=='Multi' or kind_of_prediction=='Regression' :
+elif kind_of_prediction=='Multi':
     class_list_at0=multi_class_tumor_load[merged_table['TimePointNum']==0]
 else:
     raise ValueError
@@ -120,38 +128,21 @@ kf = KFold(n_splits = Constants.N_SPLITS, shuffle = True,random_state=5)
 """Get an evaluation function from the user"""
 eval_fn_name=input('Enter the evaluation function wanted \n'+'\n'.join(Constants.Evaluation_name_list)+'\n')
 
-if kind_of_prediction!='Regression':
-    percentage=[]
-    """Evaluate the KNN for various K values. """
-    for neighbors in range(1,Constants.MAX_NIGH):
-        neigh = KNeighborsClassifier(n_neighbors=neighbors,weights='distance',p=1)
-        percentage.append(Clustering.cross_validation(kf,dec_data_at0,class_list_at0,neigh,eval_fn_name)[1])
+percentage=[]
+"""Evaluate the KNN for various K values. """
+for neighbors in range(1,Constants.MAX_NIGH):
+    neigh = KNeighborsClassifier(n_neighbors=neighbors,weights='distance',p=1)
+    percentage.append(Clustering.cross_validation(kf,dec_data_at0,class_list_at0,neigh,eval_fn_name)[1])
 
 
-    """Plot the results"""
-    plt.plot(range(1,Constants.MAX_NIGH),percentage,marker='o',markersize=15,markerfacecolor='red')
-    plt.title('Data TimePoints : {TP}\nModel : {pred} KNN \nDimension_fn : {dim} \nCorrelation_threshold : {'
-'corr_TH}\nNormalization_fn : {norm}\n'.format(TP=TimePoint,pred=kind_of_prediction,
+"""Plot the results"""
+plt.plot(range(1,Constants.MAX_NIGH),percentage,marker='o',markersize=15,markerfacecolor='red')
+plt.title('Model : {pred} KNN \nDimension_fn : {dim} \nCorrelation_threshold : {'
+'corr_TH}\nNormalization_fn : {norm}\n'.format(pred=kind_of_prediction,
                                                               dim=dimension_fn_name,corr_TH=str(Constants.THRESHOLD),
                                                               norm=normalization_fn_name))
-    plt.ylabel(eval_fn_name)
-    plt.xlabel('K neighbors')
-    plt.tight_layout()
-    plt.show()
+plt.ylabel(eval_fn_name)
+plt.xlabel('K neighbors')
+plt.tight_layout()
+plt.show()
 
-    """if the user wish to perform a regression, build a SVR model and print the R2 performance"""
-
-elif kind_of_prediction=='Regression':
-    test_loss=[]
-    train_loss=[]
-    for C in [0+p/100 for p in range(1,10000)]:
-        clf= SVR(C=C, epsilon=0.1,kernel='poly',degree=3,gamma='scale')
-        result=Clustering.cross_validation(kf,dec_data_at0,class_list_at0,clf,eval_fn_name)
-        train_loss.append(result[0])
-        test_loss.append((result[1]))
-    plt.plot([0+p/100 for p in range(1,10000)],train_loss,marker='o',markersize=5,label='Train')
-    plt.plot([0+p/100 for p in range(1,10000)],test_loss,marker='o',markersize=5,label='Test')
-    plt.xlabel('regularization')
-    plt.ylabel('R2')
-    plt.legend()
-    plt.show()
