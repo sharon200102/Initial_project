4cb84f8a69a58be4c68a8931ee74e93bb84e8dfa
sharon200102 <sharon200102@gmail.com>
2020-08-05 14:22:36 +0300

    returning to the crohn project

--- Projects/Crohn_project/Main.py
+++ Projects/Crohn_project/Main.py
@@ -4,77 +4,67 @@ import pandas as pd
 import Projects.Crohn_project.Constants as Constants
 from Code import Data_loader as DL
 import Code.Plot as Plot
-import infra_functions.preprocess_grid as preprocess_grid
-#download and import the biom library
+import Preprocess.preprocess_grid as preprocess_grid
+# download and import the biom library
 from biom import load_table
 from sklearn.model_selection import cross_validate
 from sklearn.svm import SVR
 from sklearn import linear_model
 import os
+from LearningMethods.general_functions import train_test_split
 import seaborn as sns
+import pickle
+from LearningMethods.multi_model_learning import MultiModel
+
 # In this script we will use paths that are relative to the main script absolute path.
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-
 # First load the the data including the biom type file
-biom_table = load_table(os.path.join(script_dir,Constants.biom_file_path))
-mapping_table=DL.files_read_csv_fails_to_data_frame(os.path.join(script_dir,Constants.mapping_file_path),1)
-#------------------------------------------------------------------------------------------------------------------------
+biom_table = load_table(os.path.join(script_dir, Constants.biom_file_path))
+mapping_table = DL.files_read_csv_fails_to_data_frame(os.path.join(script_dir, Constants.mapping_file_path), 1)
+# ------------------------------------------------------------------------------------------------------------------------
 # visualize the rea;tionship between the categorical features and the continuous features in one plot.
-vis1=Plot.categorical_vs_numeric_features_plot(mapping_table,Constants.categorical_features_names,Constants.numerical_features_names,figsize=(15,15))
-fig,grid=vis1.map_grid(hue_dict={('Group','CRP_n'):'family_background_of_crohns'})
-for row in grid:
-    for ax in row:
-        ax.tick_params(axis="x", labelsize=6.5)
-plt.show()
+visualize = False
+if visualize == True:
+    vis1 = Plot.categorical_vs_numeric_features_plot(mapping_table, Constants.categorical_features_names,
+                                                     Constants.numerical_features_names, figsize=(15, 15))
+    fig, grid = vis1.map_grid(hue_dict={('Group', 'CRP_n'): 'family_background_of_crohns'})
+    for row in grid:
+        for ax in row:
+            ax.tick_params(axis="x", labelsize=6.5)
+    plt.show()
 # Change the taxonomy structure to fit Yoel's preprocess grid function
-taxonomy=DL.files_read_csv_fails_to_data_frame(Constants.taxonomy_page_url).drop('Confidence',axis=1)
-taxonomy=taxonomy.rename({'Taxon':'taxonomy'},axis=1).set_index('Feature ID').transpose()
+taxonomy = DL.files_read_csv_fails_to_data_frame(Constants.taxonomy_page_url).drop('Confidence', axis=1)
+taxonomy = taxonomy.rename({'Taxon': 'taxonomy'}, axis=1).set_index('Feature ID').transpose()
 # then switch the biom table to dataframe format.
-microbiome=biom_table.to_dataframe(True).transpose()
-microbiome.index.name='ID'
-#------------------------------------------------------------------------------------------------------------------------
+microbiome = biom_table.to_dataframe(True).transpose()
+microbiome.index.name = 'ID'
+# ------------------------------------------------------------------------------------------------------------------------
 
 # Add the taxonomy row to the biom dataframe (Yoel's documentation)
-merged_microbiome_taxonomy = pd.concat([microbiome,taxonomy])
-merged_microbiome_taxonomy.dropna(axis=1,inplace=True)
-dict={'taxonomy_level':6,'taxnomy_group':'mean','epsilon':1,'normalization':'log','z_scoring':False,'norm_after_rel':False,'pca':15,'std_to_delete':0}
-dec_data,merged_microbiome_taxonomy_b_pca,_ = preprocess_grid.preprocess_data(merged_microbiome_taxonomy,dict,mapping_table,'Crohn_data',False)[0:3]
-#------------------------------------------------------------------------------------------------------------------------
-# Plot Correlation
-merged_microbiome_taxonomy_b_pca_with_mapping_features=merged_microbiome_taxonomy_b_pca.merge(mapping_table[['SampleID','CRP_n']],left_index=True,right_on='SampleID').set_index('SampleID')
-target=merged_microbiome_taxonomy_b_pca_with_mapping_features['CRP_n']
-Plot.draw_rhos_calculation_figure(target,merged_microbiome_taxonomy_b_pca_with_mapping_features.drop('CRP_n',axis=1),'Correlation between bacteria and target',6,save_folder=Constants.path_of_correlation_between_bacteria_and_target)
-Plot.draw_rhos_calculation_figure(mapping_table['CRP_n'],mapping_table[Constants.mapping_file_numeric_columns_relvant_to_correlation],'Correlation between numeric features and target',6,save_folder=Constants.path_of_correlation_between_numeric_features_and_target)
-#------------------------------------------------------------------------------------------------------------------------
-#visualization
-# Merge with the mapping table to gather more info on each sample
-
-dec_data_with_mapping_features=dec_data.merge(mapping_table,left_index=True,right_on='SampleID').set_index('SampleID')
-
-Plot.relationship_between_features(dec_data,'relationship_between_features',title="relationship between features colored by group2",color=dec_data_with_mapping_features['Group2'])
-Plot.relationship_between_features(dec_data,'relationship_between_features',color=dec_data_with_mapping_features['smoking'],title="relationship between features colored by smoking")
-Plot.relationship_between_features(dec_data,'relationship_between_features',color=dec_data_with_mapping_features['family_background_of_crohns'],title="relationship between features colored by family background")
-#------------------------------------------------------------------------------------------------------------------------
-# Regression
-
-dec_data_with_no_nan=dec_data[dec_data_with_mapping_features['CRP_n']!='NA']
-crp_n_no_nan=dec_data_with_mapping_features['CRP_n'][dec_data_with_mapping_features['CRP_n']!='NA']
-test_loss = []
-train_loss = []
-range=[ 5+n/20 for n in range(1,101)]
-for C in range:
-    clf = linear_model.Ridge(alpha=C)
-    result = cross_validate(clf,dec_data_with_no_nan,crp_n_no_nan,cv=Constants.N_SPLITS,return_train_score=True)
-    train_loss.append(result['train_score'].mean())
-    test_loss.append((result['test_score'].mean()))
-fig,ax=plt.subplots()
-
-ax.plot(range, train_loss, marker='o', markersize=5, label='Train')
-ax.plot(range, test_loss, marker='o', markersize=5, label='Test')
-ax.set_xlabel('regularization')
-ax.set_ylabel('R2')
-ax.legend()
-plt.show()
-
-
+merged_microbiome_taxonomy = pd.concat([microbiome, taxonomy])
+merged_microbiome_taxonomy.dropna(axis=1, inplace=True)
+dict = {'taxonomy_level': 6, 'taxnomy_group': 'mean', 'epsilon': 1, 'normalization': 'log', 'z_scoring': False,
+        'norm_after_rel': False, 'pca': (15, "pca"), 'std_to_delete': 0}
+dec_data, merged_microbiome_taxonomy_b_pca, pca_obj, bacteria, _number_of_components = preprocess_grid.preprocess_data(
+    merged_microbiome_taxonomy, dict, mapping_table, False)
+bacteria = list(bacteria)
+# ------------------------------------------------------------------------------------------------------------------------
+# Get from the user the form of the analysis
+target_feature = input('\n What feature you would like to use as a target\n')
+# ------------------------------------------------------------------------------------------------------------------------
+if target_feature == 'Group2':
+    mapping_table.rename({target_feature: 'Tag'}, axis=1, inplace=True)
+    mapping_table = mapping_table.assign(
+        Tag=mapping_table['Tag'].transform(lambda status: Constants.active_dict.get(status, 0)))
+    dec_data, target_df = preprocess_grid.adjust_table_to_target(dec_data, mapping_table, right_on='SampleID',
+                                                                 left_index=True, remove_nan=True)
+    train_idx_list, test_idx_list = train_test_split(dec_data, target_df['Tag'], target_df['patient_No'],
+                                                     random_state=1)
+    learning_method_parameters = Constants.learning_method_parameters
+    learning_method_parameters['Bacteria'] = bacteria
+    learning_method_parameters['PCA'] = pca_obj
+    mm = MultiModel(learning_method_parameters)
+    mm.fit(dec_data, target_df['Tag'], train_idx_list, test_idx_list, train_idx_list, test_idx_list)
+else:
+    raise Exception('The target feature {target} is not supported'.format(target=target_feature))
